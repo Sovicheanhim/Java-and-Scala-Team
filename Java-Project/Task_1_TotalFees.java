60c324e3d8a9edd381303bcb5084217f09f36a6b
Layhort-Iv <45727513+Layhort-Iv@users.noreply.github.com>
2019-09-12 16:04:32 +0700

    Total Fees

--- Java-Project/Task_1_TotalFees.java
+++ Java-Project/Task_1_TotalFees.java
@@ -3,25 +3,25 @@ import java.util.*;
 public class Task_1_TotalFees {
     public static void main(String[] args) {
         int totalBookPrize=0,totalCreditHourPrize,creditHour,athleticHour,totalAthleticPrize;
-        Scanner aa= new Scanner(System.in);
+        Scanner input= new Scanner(System.in);
         System.out.println("Enter the number of credit hour :");
-        creditHour = aa.nextInt();
+        creditHour = input.nextInt();
 
         totalCreditHourPrize = creditHour*85;
 
         System.out.println("Enter number of book :");
-        int numberOfBook = aa.nextInt();
+        int numberOfBook = input.nextInt();
         ArrayList<Integer> bookPrize = new ArrayList<Integer>(numberOfBook);
         if(numberOfBook == 1){
             System.out.println("Enter the book prize");
-            bookPrize.add(aa.nextInt());
+            bookPrize.add(input.nextInt());
             for(int i:bookPrize)
                 totalBookPrize+=i;
         }
         else if(numberOfBook>1){
             for(int i =1 ;i<=numberOfBook;i++){
                 System.out.println("Enter the prize for book "+i);
-                bookPrize.add(aa.nextInt());
+                bookPrize.add(input.nextInt());
             }
             for(int i:bookPrize)
                 totalBookPrize+=i;
@@ -30,12 +30,12 @@ public class Task_1_TotalFees {
             System.out.println("We don't accept the negative value :)");
 
         System.out.println("Did you do athletic? Enter true or false !");
-        boolean answer = aa.nextBoolean();
+        boolean answer = input.nextBoolean();
 
         if(answer)
         {
             System.out.println("Enter the number of athletic hours :");
-            athleticHour = aa.nextInt();
+            athleticHour = input.nextInt();
             totalAthleticPrize = athleticHour * 65;
 
             System.out.println("The total prize is "+calculateTotalPrize(totalCreditHourPrize,totalBookPrize,totalAthleticPrize));
