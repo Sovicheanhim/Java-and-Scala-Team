cf59668ea78f43c5a44f2b586f418fac513cace4
SOKHUONG <uonsokhuong18@kit.edu.kh>
2019-09-14 22:43:00 +0700

    changed Scala/task1/ex1_fee.scala and added Scala/task1/ex2.scala, Scala/task3/ex1_swimming.scala, and Scala/task5/ex1_tag.scala

---
+++ Uon Sokhuong/Scala/task3/ex1_swimming.scala
@@ -0,0 +1,29 @@
+import scala.io.StdIn
+
+object swimming{
+    def check(prompt:String):Float={
+        println(prompt)
+        while(true){
+            try{
+                val input=StdIn.readFloat()
+                return if(input>0) input else check(prompt)
+            }catch{
+                case x: Exception =>println("Invalid Input!    \n"+prompt)
+            }
+        }
+        return 0
+    }
+    def main(args:Array[String]){
+        val length=check("Please enter the length of the pool in feet:")
+        val width=check("Please enter the width of the pool in feet:")
+        val depth=check("Please enter the average depth of the pool in feet:")
+        
+        val pool_valume=length*width*depth
+        val pool_capacity=pool_valume*7.5
+        val time_minutes=pool_capacity/50
+        val time_hour=time_minutes/60
+        val total_fee=75+time_hour*8
+
+        println("Total fee for the service is: $"+s"${total_fee}")
+    }
+}
