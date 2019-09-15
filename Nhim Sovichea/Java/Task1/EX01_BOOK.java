7c63a98ec9538b9e29a575b8f2803b4a3c2f2f1d
Sovichea Nhim <nhimsovichea18@kit.edu.kh>
2019-09-15 23:42:05 +0700

    Added task1 for Java

--- Nhim Sovichea/Java/Task1/EX01_BOOK.java
+++ Nhim Sovichea/Java/Task1/EX01_BOOK.java
@@ -1,6 +1,16 @@
-class EX01_BOOK{
+package Task1;
+
+import java.util.Scanner;
+
+public class EX01_BOOK {
     public static void main(String[] args) {
-        System.out.println("Enter the amount of Money in Dollars :");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of credit hours for this semester: ");
+        double creditsHour = input.nextDouble();
+        System.out.println("Enter the amount of money spent on books: ");
+        double bookCost = input.nextDouble();
+        double totalCost = (creditsHour*85)+bookCost+65;
+        System.out.println("Your school fee is "+creditsHour*85+"$\nYour books'cost is "+bookCost+"$\nYour athletic fee is 65$\nYour total cost "+totalCost+"$");
 
     }
 }
